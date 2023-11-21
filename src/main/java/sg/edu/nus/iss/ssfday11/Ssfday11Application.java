ea8ed3f0aded379b0b5498130d048f30cbbc906b
Yong Le <ylsong152@gmail.com>
2023-11-21 10:09:19 +0800

    Initial commit

---
+++ src/main/java/sg/edu/nus/iss/ssfday11/Ssfday11Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssfday11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssfday11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssfday11Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/ssfday11/Ssfday11ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssfday11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssfday11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
